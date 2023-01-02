b74d167997f7ffd7362a1e613b4d0ca3c99f89dd
Qiao Wang <wangqiao11@hotmail.com>
2023-01-02 16:50:24 +0800

    Add language settings and data sync operation to settings page.

---
+++ PocketClient.Desktop/Converters/LanguageToBooleanConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.UI.Xaml.Data;
+
+namespace PocketClient.Desktop.Converters;
+
+public class LanguageToBooleanConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        if (parameter is string)
+        {
+            return value.Equals(parameter);
+        }
+        
+        return false;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language) => throw new NotImplementedException();
+}
