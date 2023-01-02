735093bc76df004af2346c4c2bc86f7ee2c728c9
Qiao Wang <wangqiao11@hotmail.com>
2023-01-02 17:01:43 +0800

    Add language settings and data sync operation to settings page. (#9)

--- PocketClient.Desktop/ViewModels/SettingsViewModel.cs
+++ PocketClient.Desktop/ViewModels/SettingsViewModel.cs
@@ -10,14 +10,19 @@ using PocketClient.Desktop.Contracts.Services;
 using PocketClient.Desktop.Helpers;
 
 using Windows.ApplicationModel;
+using Windows.Globalization;
 
 namespace PocketClient.Desktop.ViewModels;
 
 public class SettingsViewModel : ObservableRecipient
 {
     private readonly IThemeSelectorService _themeSelectorService;
+    private readonly IPocketDbService _pocketDbService;
+
     private ElementTheme _elementTheme;
     private string _versionDescription;
+    private string _language;
+    private bool _syncing;
 
     public ElementTheme ElementTheme
     {
@@ -31,26 +36,47 @@ public class SettingsViewModel : ObservableRecipient
         set => SetProperty(ref _versionDescription, value);
     }
 
+    public string Language
+    {
+        get => _language;
+        set => SetProperty(ref _language, value);
+    }
+
+    public bool Syncing
+    {
+        get => _syncing;
+        set => SetProperty(ref _syncing, value);
+    }
+
     public ICommand SwitchThemeCommand
     {
         get;
     }
 
-    public SettingsViewModel(IThemeSelectorService themeSelectorService)
+    public ICommand SwitchLanguageCommand
+    {
+        get;
+    }
+
+    public ICommand SyncDataCommand
+    {
+        get;
+    }
+
+    public SettingsViewModel(IThemeSelectorService themeSelectorService, IPocketDbService pocketDbService)
     {
         _themeSelectorService = themeSelectorService;
-        _elementTheme = _themeSelectorService.Theme;
-        _versionDescription = GetVersionDescription();
+        _pocketDbService = pocketDbService;
 
-        SwitchThemeCommand = new RelayCommand<ElementTheme>(
-            async (param) =>
-            {
-                if (ElementTheme != param)
-                {
-                    ElementTheme = param;
-                    await _themeSelectorService.SetThemeAsync(param);
-                }
-            });
+        _elementTheme = _themeSelectorService.Theme;
+        _language = GetPreferredLanguage();
+        _versionDescription = GetVersionDescription();
+        _syncing = false;
+
+        SwitchThemeCommand = new AsyncRelayCommand<ElementTheme>(SwitchTheme);
+        SwitchLanguageCommand = new RelayCommand<string>(SwitchLanguage);
+        SyncDataCommand = new AsyncRelayCommand(SyncData);
+        _pocketDbService = pocketDbService;
     }
 
     private static string GetVersionDescription()
@@ -69,5 +95,59 @@ public class SettingsViewModel : ObservableRecipient
         }
 
         return $"{"AppDisplayName".GetLocalized()} - {version.Major}.{version.Minor}.{version.Build}.{version.Revision}";
+    }
+
+    private static string GetPreferredLanguage()
+    {
+        if (string.IsNullOrEmpty(ApplicationLanguages.PrimaryLanguageOverride))
+        {
+            return "Default";
+        }
+
+        return ApplicationLanguages.PrimaryLanguageOverride;
+    }
+
+    private async Task SwitchTheme(ElementTheme theme)
+    {
+        if (ElementTheme != theme)
+        {
+            ElementTheme = theme;
+            await _themeSelectorService.SetThemeAsync(theme);
+        }
+    }
+
+    private void SwitchLanguage(string? lang)
+    {
+        if (string.IsNullOrEmpty(lang) || Language == lang)
+        {
+            return;
+        }
+        
+        Language = lang;
+
+        if (lang == "Default")
+        {
+            ApplicationLanguages.PrimaryLanguageOverride = "";
+        }
+        else
+        {
+            ApplicationLanguages.PrimaryLanguageOverride = lang;
+        }
+    }
+
+    private async Task SyncData()
+    {
+        try
+        {
+            Syncing = true;
+            await _pocketDbService.SyncItemsAsync(fullSync: true, force: true);
+        }
+        catch (Exception ex)
+        {
+            await App.MainWindow.ShowMessageDialogAsync(ex.Message);
+        }
+        finally { 
+            Syncing = false; 
+        }
     }
 }
