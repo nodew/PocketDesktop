fb916f8b4f791370848e87539b2c5eed3a2f0b8a
Qiao Wang <wangqiao11@hotmail.com>
2023-01-31 22:52:54 +0800

    format code

--- PocketClient.Core/Data/DesignTimeDbContextFactory.cs
+++ PocketClient.Core/Data/DesignTimeDbContextFactory.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore.Design;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
 
 namespace PocketClient.Core.Data;
 
